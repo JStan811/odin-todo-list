ea01c402e1f34cf411d1d57b14bf313a5617642a
Jake Standley <jakestandley95@gmail.com>
2022-02-08 12:13:26 -0800

    Update initial page render function
    At this point I've got the main heading, the list container, and the task container rendering on page load. I'm using a 6x6 grid for the page layout, although I may update that to a 12x12 for more flexibility. Right now, the tast list is too large imo when spanning 2 columns and too small when spanning 1. It may look better spanning 3 / 12.

--- src/view.js
+++ src/view.js
@@ -1,10 +1,44 @@
 const renderPage = () => {
+  createGridContainer();
+  createMainHeading();
+  createListContainer();
+  createTaskContainer();
+}
+
+const createGridContainer = () => {
   const body = document.querySelector('body');
 
   const gridContainer = document.createElement('div');
   gridContainer.classList.add('grid');
 
   body.appendChild(gridContainer);
+}
+
+const createMainHeading = () => {
+  const heading = document.createElement('h1');
+  const gridContainer = document.querySelector('.grid');
+
+  heading.textContent = 'Todo List';
+
+  gridContainer.appendChild(heading);
+}
+
+const createListContainer = () => {
+  const listContainer = document.createElement('div');
+  const gridContainer = document.querySelector('.grid');
+
+  listContainer.classList.add('list-container');
+
+  gridContainer.appendChild(listContainer);
+}
+
+const createTaskContainer = () => {
+  const taskContainer = document.createElement('div');
+  const gridContainer = document.querySelector('.grid');
+
+  taskContainer.classList.add('task-container');
+
+  gridContainer.appendChild(taskContainer);
 }
 
 export {
